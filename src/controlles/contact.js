d7ae5cefd4c88886cb2b5e0bd56e56a601e30b6a
CaioAugusto <caioaugustov@gmail.com>
2020-01-17 18:59:17 -0300

    Create method put contact

--- src/controlles/contact.js
+++ src/controlles/contact.js
@@ -14,4 +14,29 @@ router.post('/', async (req, res) => {
   res.send({ data: contact })
 })
 
+router.get('/', async (req, res) => {
+  const contact = await Contact.find();
+
+  res.send({ data: contact })
+})
+
+router.put('/', async (req, res) => {
+  const { id } = req.query;
+  const { nome, email, telefone, celular } = req.body;
+
+  if(!id) res.status.send({ error: "Paramentro 'id' não está definido" })
+  if(!nome && !telefone && !celular && !email) res.status(400).send({ error: "Nenhuma informação enviada!" })
+
+  const contact = await Contact.findById(id);
+  
+  if(contact.nome == nome && 
+    contact.email == email && 
+    contact.telefone == telefone && 
+    contact.celular == celular) res.status(400).send({ error: "Nenhuma alteração" })
+
+  const UpdateContact = await Contact.update(req.body);
+
+  if(UpdateContact.nModified > 0) res.send({ data: req.body })
+})
+
 module.exports = app => app.use('/contact', router)
